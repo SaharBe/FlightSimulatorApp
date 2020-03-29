f01120c239a5637e88765efcbc05e2d2f073489e
SaharBe <57907970+SaharBe@users.noreply.github.com>
2020-03-29 16:47:53 +0300

    Add files via upload

---
+++ Iplane.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FlightSimulatorApp
+{
+    interface Iplane
+    {
+        void connect(string ip, int port);
+        void disconnect();
+        void start();
+        double LeftSonar
+        {
+            set;
+            get;
+        }
+        double CenterSonar
+        {
+            set;
+            get;
+        }
+
+        double RightSonar
+        {
+            set;
+            get;
+        }
+        byte[][] CamView
+        {
+            set;
+            get;
+        }
+    }
+}
